d11e591fb7c2c70480a3e9b15b0b221ef5d61a83
shubhdesai1611 <126991856+shubhdesai1611@users.noreply.github.com>
2023-11-29 00:21:39 -0500

    ignored node_modules

--- app.js
+++ app.js
@@ -5,10 +5,29 @@ import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 import AssignmentRoutes from "./assignments/routes.js";
+import mongoose from "mongoose";
+mongoose.connect("mongodb://localhost:27017/kanbas");
+import UserRoutes from "./users/routes.js";
 import "dotenv/config";
+import session from "express-session";
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
+const sessionOptions = {
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+app.use(session(sessionOptions));
+
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
